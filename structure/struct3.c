e1e34f696a14dfad6b1148e0924284c9c45101fb
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2025-05-27 10:06:51 +0530

    pointer with structre

--- structure/struct3.c
+++ structure/struct3.c
@@ -12,7 +12,6 @@ int main()
     struct student s1={"shadab",1,435};
     struct student *ptr;
     ptr = &s1;
-    printf("%s %d %.2f",ptr->name,ptr->rollno,ptr->marks);
-    
+    printf("%s %d %.2f",ptr->name,ptr->rollno,ptr->marks);    
     return 0;
 }
