56cb133d84bb7fd852aabe42f5173a624d6f977b
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2025-05-27 10:01:27 +0530

    pointer with structure

---
+++ structure/struct3.c
@@ -0,0 +1,18 @@
+//pointer to structure
+#include<stdio.h>
+struct student
+{
+    char name[20];
+    int rollno;
+    float marks;
+};
+
+int main()
+{
+    struct student s1={"shadab",1,435};
+    struct student *ptr;
+    ptr = &s1;
+    printf("%s %d %.2f",ptr->name,ptr->rollno,ptr->marks);
+    
+    return 0;
+}
